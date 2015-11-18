8fb45a9b3715371bf5e0588e5e9f92fe40433fd7
Andrey Bondar <Alanai1995@gmail.com>
2015-11-18 11:34:54 +0300

    something wrong

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import unittest
-import urlparse
+from selenium.webdriver.support import expected_conditions as EC
+from urlparse import urlparse
 import os
 
 
 class Page(object):
     BASE_URL = "http://mail.ru"
-    URI = ""
 
     def __init__(self, driver):
         self.driver = driver
 
     def open(self):
-        url = urlparse.urljoin(self.BASE_URL, self.URI)
-        self.driver.get(url)
-
-    def go_back_to_main(self):
         self.driver.get(self.BASE_URL)
 
     @property
@@ -32,11 +27,20 @@ class Page(object):
     def top_bar_form(self):
         return TopBarForm(self.driver)
 
+    @property
+    def search_form(self):
+        return SearchForm(self.driver)
+
+    @property
+    def auth_popup_form(self):
+        return AuthPopupForm(self.driver)
+
 
 class AuthForm(object):
     LOGIN_INPUT = '//*[@id="mailbox__login"]'
     PASSWORD_INPUT = '//*[@id="mailbox__password"]'
     SUBMIT_BUTTON = '//*[@id="mailbox__auth__button"]'
+    MAIL_RU_REF = '//*[@id="portal-headline"]/table/tbody/tr/td[1]/a[1]'
 
     def __init__(self, driver):
         self.driver = driver
@@ -49,6 +53,22 @@ class AuthForm(object):
 
     def submit(self):
         self.driver.find_element_by_xpath(self.SUBMIT_BUTTON).click()
+        WebDriverWait(self.driver, 5).until(
+            lambda driver: EC.presence_of_element_located(driver.find_element_by_xpath(self.MAIL_RU_REF))
+        )
+
+
+class AuthPopupForm(AuthForm):
+    LOGIN_INPUT = '//*[@id="ph_login"]'
+    PASSWORD_INPUT = '//*[@id="ph_password"]'
+    SUBMIT_BUTTON = '//*[@id="x-ph__authForm__popup"]/div[3]/div/div[1]/span/span/input'
+    LOGIN_BUTTON = '//*[@id="PH_authLink"]'
+
+    def open_login_popup(self):
+        WebDriverWait(self.driver, 5).until(
+            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.LOGIN_BUTTON))
+        )
+        self.driver.find_element_by_xpath(self.LOGIN_BUTTON).click()
 
 
 class UserInfoForm(object):
@@ -75,10 +95,39 @@ class TopBarForm(object):
         )
 
 
+class SearchForm(object):
+    SEARCH_INPUT = '//*[@id="q"]'
+    SEARCH_SUBMIT = '//*[@id="search__button__wrapper__field"]'
+    RESULT_SEARCH_INPUT = '//*[@id="q"]'  # Don't know if i should test this
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    def set_search_field(self, search_string):
+        self.driver.find_element_by_xpath(self.SEARCH_INPUT).send_keys(search_string)
+
+    def submit_search(self):
+        self.driver.find_element_by_xpath(self.SEARCH_SUBMIT).click()
+        WebDriverWait(self.driver, 5).until(
+            lambda driver: EC.presence_of_element_located(driver.find_element_by_xpath(self.RESULT_SEARCH_INPUT))
+        )
+
+    def get_result_search_string(self):
+        return WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.find_element_by_xpath(self.RESULT_SEARCH_INPUT).get_attribute('value').encode('utf-8')
+        )
+
+
 class MainPageTest(unittest.TestCase):
+    SEARCH_INPUT = '//*[@id="q"]'
     login = "tester-mega"
     password = os.environ['MAIL_TEST_PASS']
     user_email = "tester-mega@mail.ru"
+    search_string = "test"
+    search_url = "http://go.mail.ru/"
 
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -86,7 +135,7 @@ class MainPageTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def testUserLogin(self):
+    def test_user_login(self):
         main_page = Page(self.driver)
         main_page.open()
 
@@ -95,7 +144,45 @@ class MainPageTest(unittest.TestCase):
         auth_form.set_password(self.password)
         auth_form.submit()
 
-        main_page.go_back_to_main()
+        main_page.open()
+
+        user_info_form = main_page.user_info_form
+        user_email = user_info_form.get_user_email()
+        self.assertEquals(user_email, self.user_email)
+
+        top_bar_form = main_page.top_bar_form
+        user_email = top_bar_form.get_user_email()
+        self.assertEquals(user_email, self.user_email)
+
+    def test_search(self):
+        main_page = Page(self.driver)
+        main_page.open()
+        WebDriverWait(self.driver, 5).until(
+            lambda driver: EC.presence_of_element_located(driver.find_element_by_xpath(self.SEARCH_INPUT))
+        )
+
+        search_form = main_page.search_form
+        search_form.set_search_field(self.search_string)
+        search_form.submit_search()
+        result_search_string = search_form.get_result_search_string()
+
+        full_search_url = search_form.get_current_url()
+        full_search_url = urlparse(full_search_url)
+        search_url = '{uri.scheme}://{uri.netloc}/'.format(uri=full_search_url)
+
+        self.assertEquals(result_search_string, self.search_string)
+        self.assertEquals(search_url, self.search_url)
+
+    def testLoginByPopup(self):
+        main_page = Page(self.driver)
+        main_page.open()
+
+        auth_form = main_page.auth_popup_form
+        auth_form.set_login(self.login)
+        auth_form.set_password(self.password)
+        auth_form.submit()
+
+        main_page.open()
 
         user_info_form = main_page.user_info_form
         user_email = user_info_form.get_user_email()
