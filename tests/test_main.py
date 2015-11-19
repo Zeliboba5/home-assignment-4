0f6043defd58d1b28dc6454bbc2f946efe8f6078
Andrey Bondar <Alanai1995@gmail.com>
2015-11-19 15:21:24 +0300

    debug test case with errors

--- tests/test_main.py
+++ tests/test_main.py
@@ -169,8 +169,12 @@ class SearchForm(object):
             lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.SEARCH_SUBMIT))
         )
         self.driver.find_element_by_xpath(self.SEARCH_SUBMIT).click()
-        WebDriverWait(self.driver, 5).until(
+        WebDriverWait(self.driver, 5).until(  # This wait is to ensure that page has changed, because search
             lambda driver: EC.presence_of_element_located(driver.find_element_by_xpath(self.SEARCH_REF))
+            # input have same path on all pages
+        )
+        WebDriverWait(self.driver, 5).until(
+            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.SEARCH_INPUT))
         )
 
     def get_result_search_string(self):  # Don't know if i should test this
@@ -191,7 +195,7 @@ class MainPageTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4444/wd/hub',
-            desired_capabilities=DesiredCapabilities.FIREFOX,
+            desired_capabilities=DesiredCapabilities.CHROME,
         )
 
     def tearDown(self):
@@ -285,7 +289,7 @@ class MainPageTest(unittest.TestCase):
 
         blocks_number = 4
 
-        for i in range(2, blocks_number):  # starts from 2 because of unique main page layout
+        for i in range(2, blocks_number + 1):  # starts from 2 because of unique main page layout
             self.assertEquals(news_form.get_news_block_classes(i), chosen_block_class)
             news_form.click_news_block_button(i)
             self.assertEquals(news_form.get_news_block_classes(i), not_chosen_block_class)
@@ -295,7 +299,7 @@ class MainPageTest(unittest.TestCase):
 
         blocks_number = 7
 
-        for i in range(2, blocks_number):
+        for i in range(2, blocks_number + 1):
             self.assertEquals(news_form.get_news_block_classes(i), chosen_block_class)
             news_form.click_news_block_button(i)
             self.assertEquals(news_form.get_news_block_classes(i), not_chosen_block_class)
@@ -324,3 +328,61 @@ class MainPageTest(unittest.TestCase):
         self.assertEquals(top_bar_form.get_popup_classes(), popup_opened_classes_full_size)
         top_bar_form.trigger_popup()
         self.assertEquals(top_bar_form.get_popup_classes(), popup_closed_classes)
+
+
+class DebugPageTest(unittest.TestCase):
+    SEARCH_INPUT = '//*[@id="q"]'
+    login = "tester-mega"
+    password = os.environ['MAIL_TEST_PASS']
+    wrong_password = "qwerty123"
+    user_email = "tester-mega@mail.ru"
+    search_string = "test"
+    search_url = "http://go.mail.ru/"
+
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME,
+        )
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_search(self):
+        main_page = Page(self.driver)
+        main_page.open()
+        WebDriverWait(self.driver, 5).until(
+            lambda driver: EC.visibility_of(driver.find_element_by_xpath(self.SEARCH_INPUT))
+        )
+
+        search_form = main_page.search_form
+        search_form.set_search_field(self.search_string)
+        search_form.submit_search()
+        result_search_string = search_form.get_result_search_string()
+
+        full_search_url = search_form.get_current_url()
+        full_search_url = urlparse(full_search_url)
+        search_url = '{uri.scheme}://{uri.netloc}/'.format(uri=full_search_url)
+
+        self.assertEquals(result_search_string, self.search_string)
+        self.assertEquals(search_url, self.search_url)
+
+    def test_login_by_popup(self):
+        main_page = Page(self.driver)
+        main_page.open()
+
+        auth_form = main_page.auth_popup_form
+        auth_form.open_login_popup()
+        auth_form.set_login(self.login)
+        auth_form.set_password(self.password)
+        auth_form.submit()
+
+        main_page.open()
+
+        user_info_form = main_page.user_info_form
+        user_email = user_info_form.get_user_email()
+        self.assertEquals(user_email, self.user_email)
+
+        top_bar_form = main_page.top_bar_form
+        user_email = top_bar_form.get_user_email()
+        self.assertEquals(user_email, self.user_email)
