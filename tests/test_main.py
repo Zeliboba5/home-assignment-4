f3cc2e66bb0da1e5416edf6563a49ceb2846cbcb
Andrey Bondar <Alanai1995@gmail.com>
2015-11-20 15:27:01 +0300

    logout and refactor

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,186 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 import unittest
 from selenium.webdriver.support import expected_conditions as EC
 from urlparse import urlparse
+from page_object import Page
 import os
-
-
-class Page(object):
-    BASE_URL = "http://mail.ru"
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def open(self):
-        self.driver.get(self.BASE_URL)
-
-    def set_small_screen_size(self):
-        width = 1000  # 1150 and 1250 are border lines between changes in design
-        height = 700  # doesn't affect functionality
-
-        self.driver.set_window_size(width, height)
-
-    def set_full_screen_size(self):
-        width = 1300  # 1150 and 1250 are border lines between changes in design
-        height = 700  # doesn't affect functionality
-
-        self.driver.set_window_size(width, height)
-
-    @property
-    def auth_form(self):
-        return AuthForm(self.driver)
-
-    @property
-    def user_info_form(self):
-        return UserInfoForm(self.driver)
-
-    @property
-    def top_bar_form(self):
-        return TopBarForm(self.driver)
-
-    @property
-    def search_form(self):
-        return SearchForm(self.driver)
-
-    @property
-    def auth_popup_form(self):
-        return AuthPopupForm(self.driver)
-
-    @property
-    def news_form(self):
-        return NewsForm(self.driver)
-
-
-class AuthForm(object):
-    LOGIN_INPUT = '//*[@id="mailbox__login"]'
-    PASSWORD_INPUT = '//*[@id="mailbox__password"]'
-    SUBMIT_BUTTON = '//*[@id="mailbox__auth__button"]'
-    MAIL_RU_REF = '//*[@id="portal-menu"]/div[2]/div/div[1]/div[1]/div/div/a/img'
-    ERROR_MESSAGE_PATH = '//*[@id="LoginExternal"]/div[1]/div[3]'
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def set_login(self, login):
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.LOGIN_INPUT))
-        )
-        self.driver.find_element_by_xpath(self.LOGIN_INPUT).send_keys(login)
-
-    def set_password(self, password):
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.PASSWORD_INPUT))
-        )
-        self.driver.find_element_by_xpath(self.PASSWORD_INPUT).send_keys(password)
-
-    def submit(self):
-        self.driver.find_element_by_xpath(self.SUBMIT_BUTTON).click()
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.presence_of_element_located(driver.find_element_by_xpath(self.MAIL_RU_REF))
-        )
-
-    def get_error_message_classes(self):
-        return self.driver.find_element_by_xpath(self.ERROR_MESSAGE_PATH).get_attribute("class").split()
-
-
-class AuthPopupForm(AuthForm):
-    LOGIN_INPUT = '//*[@id="ph_login"]'
-    PASSWORD_INPUT = '//*[@id="ph_password"]'
-    SUBMIT_BUTTON = '//*[@id="x-ph__authForm__popup"]/div[3]/div/div[1]/span/span/input'
-    LOGIN_BUTTON = '//*[@id="PH_authLink"]'
-
-    def open_login_popup(self):
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.LOGIN_BUTTON))
-        )
-        self.driver.find_element_by_xpath(self.LOGIN_BUTTON).click()
-
-
-class NewsForm(object):
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_news_block_classes(self, span_index):
-        news_block_path = '//*[@id="news__wrap"]/div[' + str(span_index) + ']'
-        return self.driver.find_element_by_xpath(news_block_path).get_attribute("class").split()
-
-    def click_news_block_button(self, span_index):
-        news_block_button = '//*[@id="news"]/div[1]/table/tbody/tr/td[' + str(span_index) + ']'
-        self.driver.find_element_by_xpath(news_block_button).click()
-
-    def set_news_block_to_default(self):
-        news_block_button = '//*[@id="news"]/div[1]/table/tbody/tr/td[1]'
-        self.driver.find_element_by_xpath(news_block_button).click()
-
-
-class UserInfoForm(object):
-    EMAIL_FORM = '//*[@id="js-mailbox-user"]'
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_user_email(self):
-        return WebDriverWait(self.driver, 10).until(
-            lambda driver: driver.find_element_by_xpath(self.EMAIL_FORM).text
-        )
-
-
-class TopBarForm(object):
-    EMAIL_FORM = '//*[@id="PH_user-email"]'
-    POPUP_PATH = '//*[@id="PH_projectsMenu"]'
-    POPUP_BUTTON = '//*[@id="PH_projectsMenu_button"]'
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_user_email(self):
-        return WebDriverWait(self.driver, 10).until(
-            lambda driver: driver.find_element_by_xpath(self.EMAIL_FORM).text
-        )
-
-    def trigger_popup(self):
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.POPUP_BUTTON))
-        )
-        self.driver.find_element_by_xpath(self.POPUP_BUTTON).click()
-
-    def get_popup_classes(self):
-        return self.driver.find_element_by_xpath(self.POPUP_PATH).get_attribute("class").split()
-
-
-class SearchForm(object):
-    SEARCH_INPUT = '//*[@id="q"]'
-    SEARCH_SUBMIT = '//*[@id="search__button__wrapper__field"]'
-    SEARCH_REF = '//*[@id="logo"]/img'
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_current_url(self):
-        return self.driver.current_url
-
-    def set_search_field(self, search_string):
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.SEARCH_INPUT))
-        )
-        self.driver.find_element_by_xpath(self.SEARCH_INPUT).send_keys(search_string)
-
-    def submit_search(self):
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.SEARCH_SUBMIT))
-        )
-        self.driver.find_element_by_xpath(self.SEARCH_SUBMIT).click()
-        WebDriverWait(self.driver, 5).until(  # This wait is to ensure that page has changed, because search
-            lambda driver: EC.presence_of_element_located(driver.find_element_by_xpath(self.SEARCH_REF))
-            # input have same path on all pages
-        )
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.element_to_be_clickable(driver.find_element_by_xpath(self.SEARCH_INPUT))
-        )
-
-    def get_result_search_string(self):  # Don't know if i should test this
-        return WebDriverWait(self.driver, 10).until(
-            lambda driver: driver.find_element_by_xpath(self.SEARCH_INPUT).get_attribute('value').encode('utf-8')
-        )
 
 
 class MainPageTest(unittest.TestCase):
@@ -204,7 +26,7 @@ class MainPageTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_user_login(self):
+    def test_user_login_and_logout(self):
         main_page = Page(self.driver)
         main_page.open()
 
@@ -222,6 +44,10 @@ class MainPageTest(unittest.TestCase):
         top_bar_form = main_page.top_bar_form
         user_email = top_bar_form.get_user_email()
         self.assertEquals(user_email, self.user_email)
+
+        auth_form.logout()
+
+        self.assertTrue(auth_form.is_auth_input_present())
 
     def test_user_login_fail(self):
         error_message_classes = ('login-page__external_error' +
@@ -350,42 +176,3 @@ class DebugPageTest(unittest.TestCase):
 
     def tearDown(self):
         self.driver.quit()
-
-    def test_search(self):
-        main_page = Page(self.driver)
-        main_page.open()
-        WebDriverWait(self.driver, 5).until(
-            lambda driver: EC.visibility_of(driver.find_element_by_xpath(self.SEARCH_INPUT))
-        )
-
-        search_form = main_page.search_form
-        search_form.set_search_field(self.search_string)
-        search_form.submit_search()
-        result_search_string = search_form.get_result_search_string()
-
-        full_search_url = search_form.get_current_url()
-        full_search_url = urlparse(full_search_url)
-        search_url = '{uri.scheme}://{uri.netloc}/'.format(uri=full_search_url)
-
-        self.assertEquals(result_search_string, self.search_string)
-        self.assertEquals(search_url, self.search_url)
-
-    def test_login_by_popup(self):
-        main_page = Page(self.driver)
-        main_page.open()
-
-        auth_form = main_page.auth_popup_form
-        auth_form.open_login_popup()
-        auth_form.set_login(self.login)
-        auth_form.set_password(self.password)
-        auth_form.submit()
-
-        main_page.open()
-
-        user_info_form = main_page.user_info_form
-        user_email = user_info_form.get_user_email()
-        self.assertEquals(user_email, self.user_email)
-
-        top_bar_form = main_page.top_bar_form
-        user_email = top_bar_form.get_user_email()
-        self.assertEquals(user_email, self.user_email)
