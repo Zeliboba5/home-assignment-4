224a379d7ac77443ea5252badc7ce24ece4c143f
Andrey Bondar <Alanai1995@gmail.com>
2015-11-28 23:52:17 +0300

    last fix

--- tests/custom_asserts.py
+++ tests/custom_asserts.py
@@ -1,22 +1,23 @@
 from urlparse import urlparse
 
+
 class CustomAsserts:
     def __init__(self):
         pass
 
     def assertDomainEqual(self, full_url, expected_url):
-    	full_url = urlparse(full_url)
+        full_url = urlparse(full_url)
         domain = '{uri.scheme}://{uri.netloc}/'.format(uri=full_url)
         if domain != expected_url:
             raise AssertionError('expected_url not equal domain from full_url')
-        return true
+        return True
 
     def assertNewsChangeClass(self, blocks_number, news_form, chosen_block_class, not_chosen_block_class):
-    	for i in range(2, blocks_number + 1):  # starts from 2 because of unique main page layout
-			if (news_form.get_news_block_classes(i) != chosen_block_class):
-				raise AssertionError('news_form.get_news_block_classes(i) != chosen_block_class')     
+        for i in range(2, blocks_number + 1):  # starts from 2 because of unique main page layout
+            if (news_form.get_news_block_classes(i) != chosen_block_class):
+                raise AssertionError('news_form.get_news_block_classes(i) != chosen_block_class')
 
-			news_form.click_news_block_button(i)
+            news_form.click_news_block_button(i)
 
-			if (news_form.get_news_block_classes(i) != not_chosen_block_class):
-				raise AssertionError('news_form.get_news_block_classes(i) != not_chosen_block_class')
+            if (news_form.get_news_block_classes(i) != not_chosen_block_class):
+                raise AssertionError('news_form.get_news_block_classes(i) != not_chosen_block_class')
